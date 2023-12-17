2270531a7e15bab7e99c435ae49cd6e18c5339b6
haibowu <haibowu@hotmail.com>
2023-12-17 19:49:37 +0800

    add github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -25,8 +25,8 @@ const whenExternalScripts = (items = []) =>
     : [];
 
 export default defineConfig({
-  site: SITE.site,
-  base: SITE.base,
+  site: 'https://whb.github.io',
+  base: '/astrowind',
   trailingSlash: SITE.trailingSlash ? 'always' : 'never',
 
   output: 'static',
